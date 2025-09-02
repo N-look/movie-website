390ebdad1f5df4996ee93fa32bb65cd7d54236d6
TonyW-Y <tony.w3435tw@gmail.com>
2025-09-02 15:31:43 -0400

    Completed base

---
+++ frontend/src/components/CardList.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import CardImg from "../assets/cardimg.jpg"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import {Link} from "react-router";
+
+const CardList = ({title, category}) => {
+    const [data, setData] = useState([]);
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTZkNzc1MjE0OWYyNDRmYjA1YzUyMWYwMmIyMjA2NyIsIm5iZiI6MTc1NjcwMjI2Ni4zNTksInN1YiI6IjY4YjUyNjNhNWMzMTFkODIyMDNjZDJjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bRP4t5hi2rDThNsS4ly3iN6mjFgR9s3wwvcNnJruIec'
+  }
+};
+
+useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setData(res.results))
+    .catch(err => console.error(err));
+}, []);
+
+
+  return (
+    <div className ="text-white md:px-4">
+        <h2 className='pt-10 pb-5 text-lg font-medium'>{title}</h2>
+
+        <Swiper slidesPerView={"auto"} spaceBetween={10} className="mySwiper">
+            {data.map((item, index) => (
+                <SwiperSlide key={index} className="max-w-72">
+                    <Link to={`/movie/${item.id}`}>
+                    <img 
+                    src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} 
+                    alt="" 
+                    className='h-44 w-full object-center object-cover'
+                    />
+                    <p className='text-center pt-2'>{item.original_title}</p>
+                    </Link>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
+  )
+}
+
+export default CardList
